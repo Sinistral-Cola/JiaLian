65151a33922ba2b950977046f9562911e1cb1ca8
Sinistral-Color <2395379994@qq.com>
2022-05-14 19:24:08 +0800

    '半成品'

--- public/js/replaceOperation.js
+++ public/js/replaceOperation.js
@@ -1,17 +1,7 @@
 $(function () {
-    $(document).scroll(() => {
-        if ($(document).scrollTop() > 150) {
-            $(".contentMiddle>div:first-child>img").addClass('animated fadeInLeft')
-            $(".contentMiddle>div:last-child").addClass('animated fadeInRight')
-        }
-        if ($(document).scrollTop() > 500) {
-            $(".contentBottom>div").addClass('animated fadeInDown')
-            $(".contentBottom>ul").addClass('animated bounceIn')
-        }
-        if ($(document).scrollTop() > 900) {
-            $(".list>div").addClass('animated rollIn')
-            $(".list>li>p").addClass('animated fadeInRight')
-            $(".list>li>h3").addClass('animated fadeInDownBig')
-        }
-    })
+    $(".contentMiddleOne>ul>li").addClass('animated fadeInRightBig')
+    $(".contentMiddleTwo>ul>li").addClass('animated fadeInLeftBig')
+    $(".contentMiddleTwo>div").addClass('animated fadeInRightBig')
+    $(".contentMiddleThree>ul>li").addClass('animated fadeInRightBig')
+    $(".contentMiddleThree>div").addClass('animated fadeInLeftBig ')
 })
